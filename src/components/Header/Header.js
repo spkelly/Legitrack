a40d5e497d7379284a9c6d8b8a4c31bb13d544cc
spkelly <spkelly18@gmail.com>
2018-06-06 18:00:53 -0600

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class Header extends Component{
+  render(){
+    return(
+      <header className="header">
+        <h2 className="header__logo">
+          Legitrack
+        </h2>
+      </header>
+    );
+  }
+}
