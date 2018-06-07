f00fc0c6773f1a338d8777b719c24a367d2abc57
spkelly <spkelly18@gmail.com>
2018-06-07 12:19:50 -0600

    made basic bill heading component

---
+++ src/components/Bill/BillHeading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function(props){
+  return(
+    <div className="bill__heading">
+      <h1 className="heading__primary">{this.props.title}</h1>
+      <p className="paragraph">{this.props.number}</p>
+    </div>
+  );
+};
