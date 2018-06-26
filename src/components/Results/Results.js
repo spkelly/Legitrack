704009651b13df190df85543504c00fecf2ce147
spkelly <spkelly18@gmail.com>
2018-06-26 13:52:04 -0600

    added basic search results component

---
+++ src/components/Results/Results.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+
+class Results extends Component{
+
+
+  render(){
+    return(
+      <div> These are some Awesome search results</div>
+    )
+  }
+}
+
+export default Results;
