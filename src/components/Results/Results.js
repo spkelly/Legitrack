a4014f860fda9ee4ae4e3b01e4bc3de49726a5b5
spkelly <spkelly18@gmail.com>
2018-06-26 15:49:56 -0600

    updated Result component to display url query string

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
-
+import queyrString from 'query-string';
 
 
 class Results extends Component{
 
+  getSearchQuery(){
+    return queyrString.parse(this.props.location.search).q;
+  }
+
+  componentDidMount(){
+
+  }
 
   render(){
     return(
-      <div> These are some Awesome search results</div>
+      <div> This is the query that you are searching for: {this.getSearchQuery()} </div>
     )
   }
 }
