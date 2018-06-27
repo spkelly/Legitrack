ff971f187c5f3c7f794125011b976120113b99e8
spkelly <spkelly18@gmail.com>
2018-06-27 13:49:03 -0600

    added search callback and moved header to the top of all routes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,26 @@ import React, {Component} from 'react';
 
 import Header from '../Header/Header';
 import Promo from './Promo';
+import Search from '../search';
 
 class Home extends Component{
+
+  constructor(props){
+    super(props);
+    this.performSearch = this.performSearch.bind(this);
+  }
+
+  performSearch(term){
+    console.log(this.props);
+    this.props.history.push(`/search?q=${term}`);
+  }
   render(){
     return(
       <div className="home">
-        <Header />
+
         <Promo>
-          <h1>test</h1>
+          <label>Search for a bill</label>
+          <Search cb={this.performSearch}/>
         </Promo>
       </div>
     );
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,12 @@ import React from 'react';
 import Home from './components/Home/Home';
 import Bill from './components/Bill/Bill';
 import Results from './components/Results/Results';
-
+import Header from './components/Header/Header';
 
 const Routes = (props) => {
 	return(
 		<div>
-			
+			<Header />
 			<Route path="/search" component={ Results }></Route>
 			<Route path="/bill/:id" component = { Bill }></Route>
 			<Route exact path="/" component={ Home }></Route>
