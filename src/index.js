62f62e79af891ab19b0faa4ad36bfde7215d6d37
spkelly <spkelly18@gmail.com>
2018-06-27 13:50:41 -0600

    fixed indentation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Switch} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store';
 import Routes from './routes';
 
+import Header from './components/Header/Header';
 import './stylesheets/main.scss';
 
 const Index = () => {
-    return (
-    <Provider store={store}>
-        <BrowserRouter>
-                <Routes />
-        </BrowserRouter>
-    </Provider>
-    );
+  return (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
+  );
 };
 
 
