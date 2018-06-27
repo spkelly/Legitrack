8fd174a42f496fbd818c042b428f9daf388002b9
spkelly <spkelly18@gmail.com>
2018-06-27 13:52:06 -0600

    updated Result list to render found bills in centered list

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import queyrString from 'query-string';
 
-
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+//TODO: seperate into container 
 class Results extends Component{
 
   getSearchQuery(){
@@ -9,14 +12,42 @@ class Results extends Component{
   }
 
   componentDidMount(){
+    let query = queyrString.parse(this.props.location.search).q
+    if(query) this.props.fetchSearch(query);
+  }
 
+  renderResults(){
+    if(!_.isEmpty(this.props.searchResults)){
+      let results = this.props.searchResults.results;
+      return Object.keys(results).map((key,index)=>{
+        // TODO: filter out summary key from resutls list on backend
+        if(key!='summary'){
+        return(
+          <div className="results__item" key={index}>
+            <Link className="results__link" to={`/bill/${results[key].bill_id}`} >{results[key].title}</Link>
+          </div>
+        )
+      }
+      });
+    }
   }
 
   render(){
     return(
-      <div> This is the query that you are searching for: {this.getSearchQuery()} </div>
+      <div className="results">
+        <h1 className="results__header">Test Search Result</h1>
+        {this.renderResults()}
+
+      </div>
     )
   }
 }
 
-export default Results;
+function mapStateToProps(state, ownProps){
+  return{
+    searchResults: state.searchResults
+  }
+};  
+
+
+export default connect(mapStateToProps, actions)(Results);
