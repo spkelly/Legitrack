4f1962c1c85a842cb8daef6b114ed667490371c7
spkelly <spkelly18@gmail.com>
2018-06-29 12:44:14 -0600

    added thunk and logger middleware to redux

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import {createStore, applyMiddleware} from 'redux';
 import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import reducers from './reducers';
+import { UV_UDP_REUSEADDR } from 'constants';
 
 const store = createStore(
   reducers, 
   undefined, 
-  applyMiddleware(promise)
+  applyMiddleware(promise,thunk, logger)
 );
 
 
