bf92a7c570647d87d4a62c096c1b870dfe2de632
spkelly <spkelly18@gmail.com>
2018-06-30 14:31:42 -0600

    added loading spinner to bill component

--- src/components/Bill/Bill.js
+++ src/components/Bill/Bill.js
@@ -7,24 +7,36 @@ import BillDescription from './BillDescription';
 import BillSponsors from './BillSponsors';
 import Button from '../Button/Button';
 import _ from 'lodash';
+import {GridLoader} from 'react-spinners';
 
+// TODO: Seperate redux functionality into container
+// TODO: Clean up renderBill funtion maybe put it in presentational component
 
 class Bill extends Component {
 
+
+  //TODO: Look for way to rerender page without having to request new bill
   componentWillMount(){
-    this.props.fetchBill(this.props.match.params.id);
+      this.props.fetchBill(this.props.match.params.id);
   }
-
-
-  render(){
-    console.log('hello ', this.props);
-    return (
-      <div className="bill">
-        <button onClick={this.props.history.goBack}>back</button>
+  renderBill(){
+    if(!_.isEmpty(this.props.currentBill)){
+      return (
+        <div>
+          <button onClick={this.props.history.goBack}>back</button>
         <BillHeading title={this.props.currentBill.title} number={this.props.currentBill.number} />
         <BillDescription text={this.props.currentBill.description} />
         <BillSponsors sponsors={this.props.currentBill.sponsors} />
         <Button title="Hello World" />
+        </div>
+      )
+    }
+  }
+
+  render(){
+    return (
+      <div className="bill"> 
+        {this.props.isFetching? <GridLoader /> : this.renderBill()}
       </div>
     );
   }
@@ -32,7 +44,9 @@ class Bill extends Component {
 
 function mapStateToProps(state, ownProps){
   return{
-    currentBill: state.bill
+    currentBill: state.currentBill.content,
+    id: state.currentBill.id,
+    isFetching: state.currentBill.isFetching
   }
 };  
 
