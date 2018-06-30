fadd1887593a6a98180c8763472364c17b430dae
spkelly <spkelly18@gmail.com>
2018-06-30 14:37:49 -0600

    added new action types to bill reducer

--- src/reducers/bill_reducer.js
+++ src/reducers/bill_reducer.js
@@ -1,11 +1,17 @@
-import { FETCH_TEST, FETCH_BILL } from '../actions/types';
+import { FETCH_TEST, FETCH_BILL, REQUEST_BILL, RECIEVE_BILL } from '../actions/types';
 
 export default function(state={}, action){
   switch(action.type){
     case FETCH_TEST:
       return action.payload.data;
-    case FETCH_BILL:
-      return action.payload.data;
+    case REQUEST_BILL:
+      return Object.assign({}, state, {id:action.id, isFetching:true})
+    case RECIEVE_BILL:
+      return Object.assign({},state,{
+        id: action.id,
+        content: action.payload.data,
+        isFetching:false
+      })
     default:
       return state;
   }
