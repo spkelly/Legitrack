9708e57e31ef1e24653fb27ff1296f611ad15bbe
spkelly <spkelly18@gmail.com>
2018-07-01 12:13:29 -0600

    copied over contents from bill component

--- src/components/containers/BillContainer.js
+++ src/components/containers/BillContainer.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import * as actions from '../../actions';
 import BillHeading from './BillHeading';
 import BillDescription from './BillDescription';
 import BillSponsors from './BillSponsors';
 import Button from '../Button/Button';
 import _ from 'lodash';
+import {GridLoader} from 'react-spinners';
 
+// TODO: Seperate redux functionality into container
+// TODO: Clean up renderBill funtion maybe put it in presentational component
 
 class Bill extends Component {
 
+
+  //TODO: Look for way to rerender page without having to request new bill
   componentWillMount(){
-    if(_.isEmpty(this.props.currentBill)) this.props.fetchTest();
+      this.props.fetchBill(this.props.match.params.id);
   }
-
-
-  render(){
-    console.log('hello ', this.props);
-    return (
-      <div className="bill">
+  renderBill(){
+    if(!_.isEmpty(this.props.currentBill)){
+      return (
+        <div>
+          <button onClick={this.props.history.goBack}>back</button>
         <BillHeading title={this.props.currentBill.title} number={this.props.currentBill.number} />
         <BillDescription text={this.props.currentBill.description} />
         <BillSponsors sponsors={this.props.currentBill.sponsors} />
         <Button title="Hello World" />
+        </div>
+      )
+    }
+  }
+
+  render(){
+    return (
+      <div className="bill"> 
+        {this.props.isFetching? <GridLoader /> : this.renderBill()}
       </div>
     );
   }
@@ -30,9 +44,11 @@ class Bill extends Component {
 
 function mapStateToProps(state, ownProps){
   return{
-    currentBill: state.bill
+    currentBill: state.currentBill.content,
+    id: state.currentBill.id,
+    isFetching: state.currentBill.isFetching
   }
-};
+};  
 
 
 export default connect(mapStateToProps, actions)(Bill);
