3e9e8df7bc674c9e5d672f5f4348e3e732f29b2d
spkelly <spkelly18@gmail.com>
2018-07-18 09:26:43 -0600

    whitespace added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,15 +16,15 @@ class Home extends Component{
     console.log(this.props);
     this.props.history.push(`/search?q=${term}`);
   }
+
   render(){
     return(
       <div className="home">
-
         <Promo>
           <h1 className="heading__primary">Welcome To Legitrack</h1>
           <h2 className="heading__secondary u-margin-bottom-lg">Search any bill from the Colorado State Legislature</h2>
           <div className="u-center-text">
-            <Search cb={this.performSearch}/>
+            <Search cb={this.performSearch} />
           </div>
         </Promo>
         <About />
