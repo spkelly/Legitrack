de47817d7ce88ddc05a6cffe5e06818350a58729
spkelly <spkelly18@gmail.com>
2018-07-18 09:23:20 -0600

    removed bill Id from heading

--- src/components/Bill/BillHeading.js
+++ src/components/Bill/BillHeading.js
@@ -6,9 +6,6 @@ export default function({ title, number }){
       <h1 className="heading__primary">
         {title ? title: 'Bill Not Found'}
       </h1>
-      <p className="paragraph">
-        {number ? number: '0000' }
-      </p>
     </div>
   );
 };
