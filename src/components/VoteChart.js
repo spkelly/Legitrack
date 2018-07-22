3221e11e65f326fccd809a41f041478746ca7a86
spkelly <spkelly18@gmail.com>
2018-07-22 12:47:33 -0600

    fixed whitespace

--- src/components/VoteChart.js
+++ src/components/VoteChart.js
@@ -5,24 +5,19 @@ export default function ({vote}){
   return(
     <div className="vote__chart">
       <Pie
-
         data={
           {labels: ["Yay", "Nay", "NV", "Pass"],
           datasets:[{
             label: vote.desc,
             data:vote.data,
             backgroundColor: ['#2ecc71','#e74c3c','#f1c40f',"#95a5a6"]
-            }
-          ]
-          }
-          
+            }]
+          } 
         }
-      
       />
       <div className="vote__desc">
         <h2 className="heading__tertiary">{vote.desc}</h2>
       </div> 
-      
     </div>
   )
 }
