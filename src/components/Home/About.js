bf702fcd2f61919511fbe40218fa5a0d255730a3
spkelly <spkelly18@gmail.com>
2018-07-24 11:52:26 -0600

    removed placeholder promo text

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -6,18 +6,22 @@ export default function (props){
       <h1 className="heading__primary u-margin-bottom-lg">About</h1>
       <div className="about__wrapper">
         <div className="about__item">
-          <h1 className="heading__secondary u-margin-bottom-md">Heading</h1>
+          <h1 className="heading__secondary u-margin-bottom-md">Discover</h1>
           <p className="paragraph">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Search through active bills within the Colorado State legislature. View bill sponsers, full texts and ammendments. 
           </p>
         </div>
         <div className="about__item">
-          <h1 className="heading__secondary u-margin-bottom-md">Heading</h1>
-          <p className="paragraph">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+          <h1 className="heading__secondary u-margin-bottom-md">Learn</h1>
+          <p className="paragraph">
+            Stay up to date on current issues and learn what your state representatives are concentrating on.   
+          </p>
         </div>
         <div className="about__item">
-          <h1 className="heading__secondary u-margin-bottom-md">Heading</h1>
-          <p className="paragraph">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <h1 className="heading__secondary u-margin-bottom-md">Track</h1>
+          <p className="paragraph"> 
+            Get up to date information on votes and amendments as the bill makes its way from creation to signing.
+          </p>
         </div>
       </div>
     </div>
