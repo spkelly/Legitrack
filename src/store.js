a0eb8373999994c7fbb8d066978e7b8d5c28117f
spkelly <spkelly18@gmail.com>
2018-07-29 13:18:59 -0600

    removed unused middleware

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import {createStore, applyMiddleware} from 'redux';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import reducers from './reducers';
-import { UV_UDP_REUSEADDR } from 'constants';
 
 const store = createStore(
   reducers, 
