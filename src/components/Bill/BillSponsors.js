978b254886c28150fd61f057ae98b3e4a90a8941
spkelly <spkelly18@gmail.com>
2018-08-16 13:55:56 -0600

    changed how sponsor party was being selected

--- src/components/Bill/BillSponsors.js
+++ src/components/Bill/BillSponsors.js
@@ -4,9 +4,11 @@ import React from 'react';
 export default function({sponsors}){
   let renderSponsers= () =>{
     return sponsors.map((sponsor, index)=>{
+      let party = sponsor.party == "I"? 'bill__sponsor-ind': sponsor.party == "R"? 'bill__sponsor-rep': 'bill__sponsor-dem';
+
       return(
-        <div className={`bill__sponsor ${sponsor.party == 'R'?'bill__sponsor-rep':'bill__sponsor-dem'}`} key={index}>
-          <p className="paragraph">{sponsor.name}</p>
+        <div className={`bill__sponsor ${ party }`} key={index}>
+          <p className='paragraph'>{ sponsor.name }</p>
         </div>
       )
     });
@@ -16,7 +18,7 @@ export default function({sponsors}){
     <div className="bill__sponsors">
       <h2 className="heading__secondary">Sponsors</h2>
       <div className=" bill__box">
-      { sponsors? renderSponsers():' no sponsors found'}
+      { sponsors? renderSponsers():'no sponsors found'}
       </div>
     </div>
   )
