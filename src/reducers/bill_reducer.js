f5559209d88a36227d44ff1658b6f9d7c8c65b81
spkelly <spkelly18@gmail.com>
2018-08-16 14:00:00 -0600

    removed unused import

--- src/reducers/bill_reducer.js
+++ src/reducers/bill_reducer.js
@@ -1,13 +1,11 @@
-import { FETCH_TEST, FETCH_BILL, REQUEST_BILL, RECIEVE_BILL } from '../actions/types';
+import { REQUEST_BILL, RECIEVE_BILL } from '../actions/types';
 
 export default function(state={}, action){
   switch(action.type){
-    case FETCH_TEST:
-      return action.payload.data;
     case REQUEST_BILL:
       return Object.assign({}, state, {id:action.id, isFetching:true})
     case RECIEVE_BILL:
-      return Object.assign({},state,{
+      return Object.assign({}, state, {
         id: action.id,
         content: action.payload.data,
         isFetching:false
