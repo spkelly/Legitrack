a1f891d9d07c909acacb0158e03fbff8fdebc40c
spkelly <spkelly18@gmail.com>
2018-08-22 13:01:37 -0600

    Bill componnent now passes props to Billtracker Component

--- src/components/Bill/Bill.js
+++ src/components/Bill/Bill.js
@@ -6,14 +6,16 @@ import BillDescription from './BillDescription';
 import BillSponsors from './BillSponsors';
 import VoteContainer from '../Containers/VoteContainer';
 
-
+// TODO: Get Last updated date and event from search page
 export default class Bill extends Component {
   render(){
     return (
       <div className="bill">
         <BillHeading title={this.props.currentBill.title} number={this.props.currentBill.number} />
         <div className="bill__details">
-          <BillTracker />
+          <BillTracker  stats={this.props.currentBill.stats} 
+                        lastAction={"Govenor Signed"} 
+                        lastUpdated={"last Updated placeholder"} />
           <BillDescription text={this.props.currentBill.description} />
           <BillSponsors sponsors={this.props.currentBill.sponsors} />
           <VoteContainer votes={this.props.currentBill.votes} />
