894339d21209508ab0561fd3571345f81abfdfc1
spkelly <spkelly18@gmail.com>
2019-09-04 15:17:50 -0600

    added unit test to search reducer for REQUEST_SEARCH action

--- src/reducers/__tests__/reducers.tests.js
+++ src/reducers/__tests__/reducers.tests.js
@@ -1,11 +1,19 @@
 import billReducer from '../bill_reducer';
 import searchReducer from '../search_reducer';
+import * as types from '../../actions/types';
 
 describe('Reducers',()=>{
   describe('Search Reducer',()=>{
-
+    test('REQUEST_SEARCH action turns on isFetching flag in state',()=>{
+      let expectedState = {isFetching:true};
+      let action ={type: types.REQUEST_SEARCH};
+      let returnedState = searchReducer(undefined,action);
+      expect(returnedState).toEqual(expectedState);
+    });
+    test('RECIEVED_SEARCH action turns off isFetching flag in state',()=>{});
+    test('RECIEVED_SEARCH action adds results to state',()=>{});
   });
   describe('Bill Reducer',()=>{
-    
+
   });
 });
