1a07592c0277879b10531a1e81e77ab05cb6d7e9
spkelly <spkelly18@gmail.com>
2019-09-06 14:29:04 -0600

    Added tests for asynchronous actions

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -1,5 +1,33 @@
 import * as actions from '../index';
 import * as types from '../types';
+import configureStore from 'redux-mock-store';
+import mockBill from '../../components/Bill/__tests__/mockBill.json';
+import thunk from 'redux-thunk';
+
+import axios from 'axios';
+import http from '../../utils/http';
+import { fail } from 'assert';
+
+let middlewares = [thunk];
+
+jest.mock('axios');
+
+const mockStore = configureStore(middlewares);
+const initialState = {};
+
+
+const mockSearchSuccessResponse = (query) => ({
+  status: 200,
+  response:{}
+});
+
+const mockBillSuccessResponse = (id)=> ({
+  status: 200,
+  response:{
+    data:{}
+  }
+});
+
 describe('simple action creators',()=>{
   describe('requestBill',()=>{
     test('it should return the proper action type and payload', ()=>{
@@ -11,7 +39,7 @@ describe('simple action creators',()=>{
       expect(returnedAction).toBe(expectedAction);
     });
   });
-  
+
   describe('recieveBill',()=>{
     test('it should return the proper action type and payload', ()=>{
       let testBill = {};
@@ -46,5 +74,64 @@ describe('simple action creators',()=>{
     test('it should return the proper action type and payload', ()=>{
 
     });
+  });
+});
+
+describe('asynchronous actions', ()=>{
+  beforeEach(()=>{
+
+  });
+  afterEach(()=>{
+
+  });
+
+  test('fetchSearch dispatches correct actions', async()=>{
+    let store = mockStore(initialState);
+    let term = 'test';
+
+    axios.get.mockImplementation(()=>{
+      return Promise.resolve({
+        data:{
+          results: []
+        }
+      })
+      .catch((e)=>{
+        console.log(e);
+      });
+    });
+
+    let expectedActions = [
+      {type:types.REQUEST_SEARCH,payload:{query:'test'}},
+      {
+        type:types.RECEIVED_SEARCH,
+        payload: {
+          query: 'test',
+          data:[]
+        }
+      }
+    ]
+    await store.dispatch(actions.fetchSearch('test'));
+    return expect(store.getActions()).toEqual(expectedActions);
+  });
+  test('fetchBill Dispatches correct actions', async()=>{
+    let store = mockStore(initialState);
+    let billId = '6';
+    axios.get.mockImplementation(()=>{
+      return Promise.resolve({bill:mockBill});
+    });
+
+    let expectedActions = [
+      {type:types.REQUEST_BILL},
+      {
+        type:types.RECIEVE_BILL,
+        payload: {
+          id: 6,
+          bill: mockBill
+        }
+      }
+    ];
+
+    await store.dispatch(actions.fetchBill(billId));
+    return expect(store.getActions()).toEqual(expectedActions);
   });
 });
