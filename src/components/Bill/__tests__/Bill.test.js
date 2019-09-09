d02e71bd0a7a187705f2ce0b22e2b35b169b7eba
spkelly <spkelly18@gmail.com>
2019-09-09 09:49:10 -0600

    Added unit tests to bill and paginate components

--- src/components/Bill/__tests__/Bill.test.js
+++ src/components/Bill/__tests__/Bill.test.js
@@ -1,10 +1,36 @@
-
 import React from 'react';
 import ReactDom from 'react-dom';
+import { shallow } from 'enzyme';
 import Bill from '../Bill';
+import BillDescription from '../BillDescription';
+import BillHeading from '../BillHeading';
 import testBill from './mockBill';
 
-test('mounts',()=>{
+test('mounts', () => {
   let container = document.createElement('div');
-  ReactDom.render(<Bill currentBill={testBill}/>, container);
+  ReactDom.render(<Bill currentBill={testBill} />, container);
+});
+
+describe('<BillDescription />', () => {
+  test('renders text prop', () => {
+    let description = 'test description';
+    let wrapper = shallow(<BillDescription text={description} />);
+    expect(wrapper.find('.paragraph').text()).toEqual(description);
+  });
+  test('renders no "no description found" if text prop is undefined', () => {
+    let wrapper = shallow(<BillDescription />);
+    expect(wrapper.find('.paragraph').text()).toEqual('no description found');
+  });
+});
+
+describe('<BillHeading />', () => {
+  test('renders text prop', () => {
+    let title = 'test title';
+    let wrapper = shallow(<BillHeading title={title} />);
+    expect(wrapper.find('.heading__primary').text()).toEqual(title);
+  });
+  test('renders no "no description found" if text prop is undefined', () => {
+    let wrapper = shallow(<BillHeading />);
+    expect(wrapper.find('.heading__primary').text()).toEqual('Bill Not Found');
+  });
 });
--- src/components/Paginate/__tests__/Paginate.test.js
+++ src/components/Paginate/__tests__/Paginate.test.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import { mount } from 'enzyme';
 import Paginate from '../Paginate';
 
 let testData = [];
-for(let i = 0; i < 10; i++){
+for (let i = 0; i < 10; i++) {
   testData.push(<div key={i}>test data {i}</div>);
 }
 
-test('renders the correct amount of elements per page',()=>{
- let wrapper = mount(<Paginate itemsPerPage={5} data={testData}></Paginate>);
- expect(wrapper.find('.results__holder').children()).toHaveLength(5);
+test('renders the correct amount of elements per page', () => {
+  let wrapper = mount(<Paginate itemsPerPage={5} data={testData} />);
+  expect(wrapper.find('.results__holder').children()).toHaveLength(5);
 });
 
-test('calls handlePageChange when new page is clicked', ()=>{
+test('calls handlePageChange when new page is clicked', () => {
+  const functionSpy = jest.spyOn(Paginate.prototype,'handlePageChange');
+  let wrapper = mount(<Paginate itemsPerPage={5} data={testData} />);
+  let button = wrapper.find('.pagination__button').last();
 
+  button.simulate('click', { preventDefault: () => {} });
+  expect(functionSpy).toHaveBeenCalled();
 });
-
