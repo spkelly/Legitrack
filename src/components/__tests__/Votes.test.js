c729fc10fda0edd0b1dc537f8278515b498dc307
Sean Kelly <spkelly18@gmail.com>
2019-09-13 12:34:04 -0600

    added tests to voteContainer and voteChart

--- src/components/__tests__/Votes.test.js
+++ src/components/__tests__/Votes.test.js
@@ -2,35 +2,36 @@ import VoteList from '../VoteList';
 import VoteContainer from '../Containers/VoteContainer';
 import React from 'react';
 import VoteChart from '../VoteChart';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+
+const voteData = [
+  {
+    desc: 'test',
+    data: [7, 0, 0, 0],
+    label: 'vote 1',
+  },
+  {
+    desc: 'test',
+    data: [32, 10, 5, 1],
+    label: 'vote 2',
+  },
+  {
+    desc: 'test',
+    data: [4, 8, 4, 10],
+    label: 'vote 3',
+  },
+  {
+    desc: 'test',
+    data: [7, 11, 23, 42],
+    label: 'vote 4',
+  },
+];
 
 describe('<VoteContainer />', () => {
-  const voteData = [
-    {
-      desc: 'test',
-      data: [7, 0, 0, 0],
-      label: 'vote 1',
-    },
-    {
-      desc: 'test',
-      data: [32, 10, 5, 1],
-      label: 'vote 2',
-    },
-    {
-      desc: 'test',
-      data: [4, 8, 4, 10],
-      label: 'vote 3',
-    },
-    {
-      desc: 'test',
-      data: [7, 11, 23, 42],
-      label: 'vote 4',
-    },
-  ];
-
   test('mounts without error', () => {
     mount(<VoteContainer votes={voteData} />);
   });
+
   test('changeVote is called when vote list is clicked', () => {
     let spy = jest.spyOn(VoteContainer.prototype, 'changeVote');
     let wrapper = mount(<VoteContainer votes={voteData} />);
@@ -57,5 +58,25 @@ describe('<VoteContainer />', () => {
 
 describe('<VoteList />', () => {
   test('renders a list of current votes', () => {});
-  test('calls callback on clicking of each list item', () => {});
+  test('calls callback on clicking of each list item', () => {
+    let mockCallback = jest.fn();
+    let wrapper = shallow(
+      <VoteList votes={voteData} cb={mockCallback} selected={0} />
+    );
+    let buttons = wrapper.find('li');
+    buttons.forEach(button => {
+      button.simulate('click', { preventDefault: () => {} });
+    });
+    expect(mockCallback).toHaveBeenCalledTimes(voteData.length);
+  });
 });
+
+
+describe('<VoteChart />', ()=>{
+  test('displays vote description in tertiary heading', ()=>{
+    let wrapper = shallow(<VoteChart vote={voteData[0]} />);
+    let description = wrapper.find('.vote__desc');
+    expect(description.childAt(0).hasClass('heading__tetiary'));
+  })
+    
+})
